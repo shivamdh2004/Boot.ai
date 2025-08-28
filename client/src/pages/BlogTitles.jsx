68d8b447fce355cd585b22a4efc0e1366f8103fe
Shivam Singh Dhakar <shivamsinghdhakar@gmail.com>
2025-08-28 19:32:00 +0530

    first commit

---
+++ client/src/pages/BlogTitles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogTitles = () => {
+  return (
+    <div>
+      <h1>blog tiltes</h1>
+    </div>
+  )
+}
+
+export default BlogTitles
---
+++ client/src/pages/Community.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Community = () => {
+  return (
+    <div>
+      <h1>community page</h1>
+    </div>
+  )
+}
+
+export default Community
